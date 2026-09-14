473f561cb1016de9ca83455434f36b9866a7e404
genort <ortizleg1452@student.faytechcc.edu>
2026-09-13 23:14:10 -0400

    Delete P1LAB1.py

--- P1LAB1.py
+++
@@ -1,9 +0,0 @@
-# The first Python lab
-
-#Get first name and save as variable
-first_name = input("Enter your first name: ")
-
-last_name = input("Enter your last name: ")
-
-#use the PRINT command to show data to user
-print("Hello", first_name, last_name, "welcome to CTI-110 !")
