0eeba0cd8b1ec090df6b19d98b86ac4a93847960
genort <ortizleg1452@student.faytechcc.edu>
2026-09-13 23:14:50 -0400

    Add files via upload
    I made my python code !

---
+++ P1LAB1.py
@@ -0,0 +1,9 @@
+# The first Python lab
+
+#Get first name and save as variable
+first_name = input("Enter your first name: ")
+
+last_name = input("Enter your last name: ")
+
+#use the PRINT command to show data to user
+print("Hello, ", first_name, last_name,"!",  "welcome to CTI-110 !")
